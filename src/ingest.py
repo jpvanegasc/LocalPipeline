0d95a0f1c5a6fd58cba41c3b8fdabf768147439f
Juan Pablo Vanegas <juan.vanegas@loka.com>
2022-03-16 16:32:24 -0500

    :sparkles: Add ingest script

---
+++ src/ingest.py
@@ -0,0 +1,33 @@
+import logging
+from io import BytesIO
+from zipfile import ZipFile
+
+import requests
+
+logging.basicConfig(level=logging.INFO)
+
+BUCKET = "https://s3.amazonaws.com/tripdata"
+
+
+def main(year, month):
+    filename = f"{year}{month}-citibike-tripdata.csv"
+    logging.info(f"Requesting file '{filename}'")
+    r = requests.get(f"{BUCKET}/{filename}.zip")
+
+    if r.ok:
+        logging.info("Unzipping file")
+        with ZipFile(BytesIO(r.content)) as zf:
+            with open(f"../data/{filename}", mode="w+") as f:
+                f.write(zf.read(filename).decode("utf-8") )
+        logging.info("File saved in disk")
+    else:
+        logging.error("Unable to get file from url")
+
+
+if __name__ == "__main__":
+    import sys
+
+    year = sys.argv[1]
+    month = sys.argv[2]
+
+    main(year, month)
