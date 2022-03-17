ad758fe86ced9753d13766c5c3de40ef981ec9ce
Juan Pablo Vanegas <juan.vanegas@loka.com>
2022-03-16 23:00:43 -0500

    :sparkles: Load files to DB

---
+++ src/load.py
@@ -0,0 +1,44 @@
+import logging
+from datetime import datetime
+
+import pandas as pd
+
+from base import engine, TABLE
+
+logging.basicConfig(level=logging.INFO)
+
+
+def load(year, month):
+    filename = f"{year}{month}-citibike-tripdata.csv"
+    dates = ["started_at", "ended_at"]
+    try:
+        df = pd.read_csv(
+            f"../data/{filename}", index_col=0, parse_dates=dates, dtype="str"
+        )
+        logging.info(f"Loaded '{filename}' into DF")
+    except:
+        logging.error(f"Failed to load '{filename}' into DF", exc_info=True)
+        return
+
+    connection = engine.connect()
+
+    try:
+        df = df.to_sql(TABLE, connection, if_exists="append")
+    except Exception:
+        logging.error("DF failed to load into postgres", exc_info=True)
+    else:
+        logging.info(f"File '{filename}' loaded in Postgres")
+    finally:
+        connection.close()
+
+
+if __name__ == "__main__":
+    import sys
+
+    from base import create_table
+
+    year = sys.argv[1]
+    month = sys.argv[2]
+
+    create_table()
+    load(year, month)
