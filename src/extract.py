0d000466602a21f01a070c3f34932ead92e0fc03
Juan Pablo Vanegas <juan.vanegas@loka.com>
2022-03-24 10:51:04 -0500

    :sparkles: Filter downloaded files

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,6 @@
 import logging
 from io import BytesIO
+import os
 from zipfile import ZipFile
 
 import requests
@@ -7,17 +8,24 @@ import requests
 logging.basicConfig(level=logging.INFO)
 
 BUCKET = "https://s3.amazonaws.com/tripdata"
+LOCAL = "data"
+FILES = os.listdir(LOCAL)
 
 
 def extract(year, month):
     filename = f"{year}{month}-citibike-tripdata.csv"
+
+    if filename in FILES:
+        logging.info("File already downloaded, skipping")
+        return
+
     logging.info(f"Requesting file '{filename}'")
     r = requests.get(f"{BUCKET}/{filename}.zip")
 
     if r.ok:
         logging.info("Unzipping file")
         with ZipFile(BytesIO(r.content)) as zf:
-            with open(f"../data/{filename}", mode="w+") as f:
+            with open(f"{LOCAL}/{filename}", mode="w+") as f:
                 f.write(zf.read(filename).decode("utf-8"))
         logging.info("File saved in disk")
     else:
